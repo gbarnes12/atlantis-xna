43d9699cd70b76c9b0bebc64e8c4d3ccbfed2bdf
Finkes93 <Finkes93@0aafdf07-8f12-183c-ebce-db5cc48efb44>
2011-11-21 00:37:35 +0000

    GamePlay/GamePlayScript.cs, GamePlayView.Designer.cs and GamePlayView.cs added

---
+++ SpaceCommander/SpaceCommander/SpaceCommander/SpaceCommander/GameViews/Gameplay/GamePlayView.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameApplicationTools.Interfaces;
+using GameApplicationTools.Structures;
+using GameApplicationTools.Misc;
+using GameApplicationTools;
+using SpaceCommander.Scripts.GamePlay;
+
+namespace SpaceCommander.GameViews.Gameplay
+{
+    public partial class GamePlayView : GameView, IGameView
+    {
+        #region Private
+        bool _blocksRendering;
+        bool _blocksUpdating;
+        #endregion
+
+        #region Public
+        public bool BlocksRendering
+        {
+            get
+            {
+                return _blocksRendering;
+            }
+            set
+            {
+                this.SetActorsVisibility(Utils.ToggleBool(value));
+                this._blocksRendering = value;
+            }
+        }
+
+        public bool BlocksUpdating
+        {
+            get
+            {
+                return _blocksUpdating;
+            }
+            set
+            {
+                this.SetActorsUpdateable(Utils.ToggleBool(value));
+                this._blocksUpdating = value;
+            }
+        }
+
+        public bool BlocksInput { get; set; }
+
+        public bool BlocksLoading { get; set; }
+        #endregion
+
+
+        public GamePlayView()
+            : base("GamePlay")
+        {
+            BlocksInput = false;
+            BlocksLoading = false;
+            BlocksUpdating = false;
+            BlocksRendering = false;
+
+            ScriptManager.Instance.ExecuteScript(GamePlayScript.OnCreateEvent);
+        }
+
+        public void LoadContent(Microsoft.Xna.Framework.Content.ContentManager content)
+        {
+            RegisterActors();
+            RegisterEvents();
+            ScriptManager.Instance.ExecuteScript(GamePlayScript.OnLoadEvent);
+        }
+
+        public void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+          
+        }
+    }
+}
---
+++ SpaceCommander/SpaceCommander/SpaceCommander/SpaceCommander/Scripts/GamePlay/GamePlayScript.cs
@@ -0,0 +1,53 @@
+﻿namespace SpaceCommander.Scripts.GamePlay
+{
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using GameApplicationTools.UI;
+using GameApplicationTools.Misc;
+using GameApplicationTools;
+    using Microsoft.Xna.Framework.Media;
+
+
+    
+    public class GamePlayScript
+    {
+        public static IEnumerator<float> OnCreateEvent()
+        {
+            #if !XBOX360
+            GameApplication.Instance.GetGame().IsMouseVisible = false;
+            Logger.Instance.Write("Created GamePlayView", LogType.Info);
+            #endif
+
+            yield return 0.0f;
+        }
+
+        /// <summary>
+        /// Gets called once the game view loads it content
+        /// and just prints it to the console!
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerator<float> OnLoadEvent()
+        {
+            GameConsole.Instance.WriteLine("Loaded Content GamePlayView", Color.White);
+
+            MediaManager.Instance.AddMusic("GamePlayMusic", GameApplication.Instance.GetGame().Content.Load<Song>(GameApplication.Instance.AudioPath + "Sleep Away"));
+            yield return 1.0f;
+
+           // MediaManager.Instance.PlayMusic("GamePlayMusic");
+
+            #if !XBOX360
+            Logger.Instance.Write("Loaded Content GamePlayView", LogType.Info);
+            #endif
+
+            yield return 0.0f;
+        }
+
+        public static IEnumerator<float> OnUpdateEvent()
+        {
+            yield return 0.0f;
+        }
+    }
+}
