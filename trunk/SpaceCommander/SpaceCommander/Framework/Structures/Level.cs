9c27dcde2ab57120121605d03fd22ae4a99cf57d
gavin.barnes90@gmail.com <gavin.barnes90@gmail.com@0aafdf07-8f12-183c-ebce-db5cc48efb44>
2011-11-29 00:58:32 +0000

    build: 1.0.4350.3426 - added assembly informations - added loading possibility of a level - added a level class - added some more changes

---
+++ trunk/SpaceCommander/SpaceCommander/Framework/Structures/Level.cs
@@ -0,0 +1,27 @@
+﻿namespace GameApplicationTools.Structures
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// The basic structure of a level. 
+    /// Use this class to load or save a level file.
+    /// 
+    /// Author: Gavin Barnes
+    /// Version: 1.0
+    /// </summary>
+    public class Level
+    {
+        #region Public
+        public string Name { get; set; }
+        #endregion
+
+        public Level() { }
+
+        public void Load(string fileName) { }
+
+        public void Save() { }
+    }
+}
